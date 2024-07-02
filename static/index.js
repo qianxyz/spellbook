62486d64fe52626eac252e953cbf7e3a01d8d8a4
Qianxy <xyqian67@gmail.com>
2024-07-02 00:09:03 -0700

    feat: add slider for level range

--- static/index.js
+++ static/index.js
@@ -3,3 +3,12 @@ const backToTop = document.getElementById("back-to-top");
 window.addEventListener("scroll", () => {
   backToTop.style.display = window.scrollY > 100 ? "block" : "none";
 });
+
+var sliderC = new Slider("#slider", {
+  id: "slider",
+  min: 0,
+  max: 9,
+  range: true,
+  value: [0, 9],
+  tooltip: "hide",
+});
